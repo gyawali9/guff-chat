71edc7d0a3d91ae08f4572042255779e00b70f06
gyawali9 <roshangyawali9@gmail.com>
2025-06-25 15:40:57 +0545

    feat: user, message and conversation model

---
+++ server/models/conversation.model.ts
@@ -0,0 +1,21 @@
+import { Schema, Types, model } from "mongoose";
+
+const conversationSchema = new Schema(
+  {
+    participants: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    messages: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Message",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export default model("Conversation", conversationSchema);
---
+++ server/models/message.model.ts
@@ -0,0 +1,23 @@
+import { Schema, Types, model } from "mongoose";
+
+const messageSchema = new Schema(
+  {
+    senderId: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverId: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default model("Message", messageSchema);
